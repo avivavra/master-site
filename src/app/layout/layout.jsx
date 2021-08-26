b2d772c1e4cd7d00295e44d42dae2008a45d15a0
Aviv <aviv.avra02@gmail.com>
2021-08-27 00:12:18 +0300

    used the library for the layout

--- src/app/layout/layout.jsx
+++ src/app/layout/layout.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
 import { childrenPropType } from '../propTypes/reactPropTypes';
 import './layout.css';
 
 const Layout = ({ children }) => (
-    <div id="layout">
-        {children}
-    </div>
+    <Grid container id="layout">
+        {
+            children.map((child) => (
+                <Grid item xs={6}>
+                    {child}
+                </Grid>
+            ))
+        }
+    </Grid>
 );
 
 Layout.propTypes = {
--- src/app/mock/layoutMock.jsx
+++ src/app/mock/layoutMock.jsx
@@ -27,6 +27,34 @@ const LayoutMock = () => (
                 <input />
             </div>
         </Section>
+        <Section name="communication">
+            <div className="links">
+                <Image url="https://www.herzog.ac.il/wp-content/uploads/2017/11/whatsapp-symbol-icon-logo-vector.png" />
+                <Image url="https://www.herzog.ac.il/wp-content/uploads/2017/11/whatsapp-symbol-icon-logo-vector.png" />
+            </div>
+            <div className="actions">
+                <button type="button">Send Mail</button>
+                <button type="button">Send Whatsapp</button>
+            </div>
+            <div className="search">
+                <button type="button">Search Instagram</button>
+                <input />
+            </div>
+        </Section>
+        <Section name="communication">
+            <div className="links">
+                <Image url="https://www.herzog.ac.il/wp-content/uploads/2017/11/whatsapp-symbol-icon-logo-vector.png" />
+                <Image url="https://www.herzog.ac.il/wp-content/uploads/2017/11/whatsapp-symbol-icon-logo-vector.png" />
+            </div>
+            <div className="actions">
+                <button type="button">Send Mail</button>
+                <button type="button">Send Whatsapp</button>
+            </div>
+            <div className="search">
+                <button type="button">Search Instagram</button>
+                <input />
+            </div>
+        </Section>
     </Layout>
 );
 
