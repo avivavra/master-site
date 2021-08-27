2bf07e8719043fe1a8c600f3fec0e0cc383f8c12
Aviv <aviv.avra02@gmail.com>
2021-08-28 00:55:52 +0300

    improved the search ui code

--- src/implementations/actors/search/singleEngineSearch/singleEngineSearch.jsx
+++ src/implementations/actors/search/singleEngineSearch/singleEngineSearch.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import {
     IconButton,
     InputBase,
-    Paper,
+    Paper
 } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 import ClearIcon from '@material-ui/icons/Clear';
@@ -13,36 +13,34 @@ import './single-engine-search.css';
 const SingleEngineSearch = ({ searchEngine, autoFocus }) => {
     const input = useRef();
 
-    const performSearch = useCallback(() => {
-        searchEngine.performSearch(input.current.value);
-        input.current.value = '';
-    }, [searchEngine, input]);
-
     const clearInput = useCallback(() => {
         input.current.value = '';
     }, [input]);
 
+    const performSearch = useCallback(() => {
+        searchEngine.performSearch(input.current.value);
+        clearInput();
+    }, [searchEngine, input, clearInput]);
+
     const searchText = `Search ${searchEngine.engineName}`;
 
     return (
-        <div className="single-engine-search">
-            <Paper component="form">
-                <IconButton aria-label="search-site-link" className="search-site-link" onClick={searchEngine.emptySearch}>
-                    <img alt="site-link" src={searchEngine.logoUrl} className="search-site-link-img" />
-                </IconButton>
-                <InputBase
-                    placeholder={searchText}
-                    inputProps={{ 'aria-label': 'search google maps', ref: input }}
-                    autoFocus={autoFocus}
-                />
-                <IconButton aria-label="search" onClick={performSearch}>
-                    <SearchIcon />
-                </IconButton>
-                <IconButton color="primary" aria-label="clear" onClick={clearInput}>
-                    <ClearIcon />
-                </IconButton>
-            </Paper>
-        </div>
+        <Paper component="form" className="single-engine-search">
+            <IconButton aria-label="search-site-link" className="search-site-link" onClick={searchEngine.emptySearch}>
+                <img alt="site-link" src={searchEngine.logoUrl} className="search-site-link-img" />
+            </IconButton>
+            <InputBase
+                placeholder={searchText}
+                inputProps={{ 'aria-label': 'search google maps', ref: input }}
+                autoFocus={autoFocus}
+            />
+            <IconButton aria-label="search" onClick={performSearch}>
+                <SearchIcon />
+            </IconButton>
+            <IconButton color="primary" aria-label="clear" onClick={clearInput}>
+                <ClearIcon />
+            </IconButton>
+        </Paper>
     );
 };
 
