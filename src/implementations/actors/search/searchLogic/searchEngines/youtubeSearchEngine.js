2c223783f4f6b6868bb54d7877630d8c10bc820f
Aviv <aviv.avra02@gmail.com>
2021-08-27 16:21:05 +0300

    added navigation logic

--- src/implementations/actors/search/searchLogic/searchEngines/youtubeSearchEngine.js
+++ src/implementations/actors/search/searchLogic/searchEngines/youtubeSearchEngine.js
@@ -1,8 +1,12 @@
+import { openInNewTab } from '../../../../../utils/navigation';
+
+const LOGO_URL = 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_play_button_icon_%282013%E2%80%932017%29.svg/640px-YouTube_play_button_icon_%282013%E2%80%932017%29.svg.png';
+
 const youtubeSearchEngine = {
     engineName: 'Youtube',
-    logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_play_button_icon_%282013%E2%80%932017%29.svg/640px-YouTube_play_button_icon_%282013%E2%80%932017%29.svg.png',
+    logoUrl: LOGO_URL,
     performSearch: (text) => {
-        console.log(`searching ${text} in youtube`);
+        openInNewTab(`https://www.youtube.com/results?search_query=${text}`);
     }
 };
 
---
+++ src/utils/navigation/openInNewTab.js
@@ -0,0 +1,9 @@
+const openInNewTab = (url) => {
+    const newWindow = window.open(url, '_blank', 'noopener,noreferrer');
+
+    if (newWindow) {
+        newWindow.opener = null;
+    }
+};
+
+export default openInNewTab;
