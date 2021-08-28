1dbbb30597101de7c2a36884d94cfbaef9f78721
Aviv <aviv.avra02@gmail.com>
2021-08-28 16:47:48 +0300

    added section layout options

--- src/app/layouts/sectionsLayout/sectionsLayout.jsx
+++ src/app/layouts/sectionsLayout/sectionsLayout.jsx
@@ -1,22 +1,49 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Grid } from '@material-ui/core';
 import { childrenPropType } from '../../propTypes/reactPropTypes';
 import './sections-layout.css';
 
-const SectionsLayout = ({ children }) => (
-    <Grid container id="layout">
-        {
-            children.map((child) => (
-                <Grid item xs={6}>
-                    {child}
-                </Grid>
-            ))
-        }
-    </Grid>
-);
-
-SectionsLayout.propTypes = {
-    children: childrenPropType.isRequired
+const sectionWidths = {
+    SMALL: 'small',
+    MEDIUM: 'medium',
+    LARGE: 'large',
+    HALF_SCREEN: 'half-screen',
+    FULL_SCREEN: 'full-screen'
 };
 
+const SectionsLayout = ({ children, sectionWidth }) => {
+    const sectionWidthToSize = {
+        [sectionWidths.SMALL]: 2,
+        [sectionWidths.MEDIUM]: 3,
+        [sectionWidths.HALF_SCREEN]: 6,
+        [sectionWidths.LARGE]: 9,
+        [sectionWidths.FULL_SCREEN]: 12,
+    };
+
+    const sectionSize = sectionWidthToSize[sectionWidth];
+
+    return (
+        <Grid container id="layout">
+            {
+                children.map((child) => (
+                    <Grid item xs={sectionSize}>
+                        {child}
+                    </Grid>
+                ))
+            }
+        </Grid>
+    );
+};
+
+SectionsLayout.propTypes = {
+    children: childrenPropType.isRequired,
+    sectionWidth: PropTypes.oneOf(Object.values(sectionWidths))
+};
+
+SectionsLayout.defaultProps = {
+    sectionWidth: sectionWidths.HALF_SCREEN
+};
+
+export { sectionWidths };
 export default SectionsLayout;
